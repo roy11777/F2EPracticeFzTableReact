40a39a9f2bc095e14683f61b148af865c0142216
roy30878 <roy855023@gmail.com>
2019-08-01 22:57:29 +0800

    ENTER

--- fztable/src/pages/Mainpage.js
+++ fztable/src/pages/Mainpage.js
@@ -6,8 +6,6 @@ class Mainpage extends React.Component {
     // 預設banner開啟狀態
     this.state = {
       Info: [],
-      goAndBackDate: [],
-      contentHoverStatus: true,
     }
   }
 
@@ -16,34 +14,44 @@ class Mainpage extends React.Component {
       fetch('/data/ticketInfo.json')
         .then(res => res.json())
         .then(obj => {
-          // console.log(obj.data[0])
           this.setState({ Info: obj.data[0].data })
           console.log(this.state.Info)
-          // return obj
         })
-      // .then(dataObj => {
-      //   console.log(dataObj)
-      //   // 裝去程所有日期選項
-      //   const godateArr = []
-      //   dataObj.data[0].data.forEach(element => {
-      //     godateArr.push(element.goDate)
-      //   })
-      //   this.setState({ goAndBackDate: godateArr })
-      //   console.log(this.state.goAndBackDate)
-      // })
     } catch (e) {
-      // console.log("123")
       console.log(e)
     }
   }
 
   tableContentMouseEnter = e => {
-    console.log(e.target)
-    this.setState({ contentHoverStatus: true })
+    e.currentTarget.classList.add('hover')
   }
   tableContentMouseLeave = e => {
-    console.log(e.target)
-    this.setState({ contentHoverStatus: false })
+    e.currentTarget.classList.remove('hover')
+  }
+  // 資料,class,index
+  handleAlignInicator = (index, ele, e) => {
+    const forAllselect = document.querySelectorAll('.forAllselect')
+
+    // 不同列的變色
+    function controlAllEle() {
+      for (let i = 0; i < forAllselect.length; i++) {
+        // 在觸發變色前先將所有上一狀態的顏色清除
+        forAllselect[i].classList.remove('alignInicator', 'active')
+        // 與點到的di於不同列相同index的變色
+        if (i % 7 === index) {
+          forAllselect[i].classList.add('alignInicator')
+        }
+      }
+    }
+    controlAllEle()
+
+    // 相同列的變色
+    // 用currentTarget可選到當前綁定的元素不會選到子元素
+    e.currentTarget.classList.add('active')
+    const rowBgSelect = e.currentTarget.parentNode.childNodes
+    for (let i = 0; i < rowBgSelect.length; i++) {
+      rowBgSelect[i].classList.add('alignInicator')
+    }
   }
 
   render() {
@@ -53,10 +61,15 @@ class Mainpage extends React.Component {
           <table>
             <tbody>
               <tr>
-                <td className="category">
-                  <span>回程</span>
-                  <span>去程</span>
+                <td>
+                  <div>
+                    <div className="category">
+                      <span>回程</span>
+                      <span>去程</span>
+                    </div>
+                  </div>
                 </td>
+
                 <td>
                   <div>
                     {this.state.Info.map((ele, index) => (
@@ -73,19 +86,29 @@ class Mainpage extends React.Component {
               {this.state.Info.map((ele, index) => (
                 <tr key={index + +new Date()}>
                   {/* 一定要用TD包ele不可單純用寫在這裡 */}
-                  <td className="dateIntervalLeft">{ele.goDate}</td>
+                  <td className="dateIntervalLeft">
+                    <div>
+                      <div>
+                        <span>{ele.goDate}</span>
+                      </div>
+                    </div>
+                  </td>
                   <td>
                     <div>
                       {ele.detail.map((e, index) => (
                         <div
                           className={
                             (e.cheapest === true ? 'cheapest' : '') +
-                            ' tableContent' +
-                            (this.state.contentHoverStatus ? ' active' : ' ')
+                            ' forAllselect'
                           }
                           key={index + +new Date()}
                           onMouseEnter={this.tableContentMouseEnter}
                           onMouseLeave={this.tableContentMouseLeave}
+                          onClick={this.handleAlignInicator.bind(
+                            this,
+                            index,
+                            e
+                          )}
                         >
                           {typeof e.price === 'number' ? '$' + e.price : ''}
                           <span>
@@ -93,7 +116,7 @@ class Mainpage extends React.Component {
                               ? '起'
                               : e.price === '--'
                               ? '查看'
-                              : ''}
+                              : '一 一'}
                           </span>
                         </div>
                       ))}
