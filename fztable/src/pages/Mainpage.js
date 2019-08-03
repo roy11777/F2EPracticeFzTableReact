aa3c7edb56a83f0f40032668a6f16aadced797e8
roy30878 <roy855023@gmail.com>
2019-08-03 23:35:10 +0800

    08032200

--- fztable/src/pages/Mainpage.js
+++ fztable/src/pages/Mainpage.js
@@ -9,9 +9,10 @@ class Mainpage extends React.Component {
       // M版時每次點擊往前往後移動幾格儲存格
       slide: 1, // [number]
       // M版時一個畫面show幾格儲存格
-      show: 2, // [number]
+      show: 3, // [number]
       // 設定花多久時間移動完成
       speed: 0.3, // [number]
+      nowInfo: 1,
     }
   }
 
@@ -23,11 +24,6 @@ class Mainpage extends React.Component {
           this.setState({ Info: obj.data[0].data })
           console.log(this.state.Info)
         })
-        .then(obj => {
-          this.hadleMobleShowControl()
-        })
-
-      window.addEventListener('resize', this.hadleMobleShowControl)
     } catch (e) {
       console.log(e)
     }
@@ -58,56 +54,66 @@ class Mainpage extends React.Component {
 
     // 相同列的變色
     // 用currentTarget可選到當前綁定的元素不會選到子元素
-    e.currentTarget.classList.add('active', 'hide')
+    e.currentTarget.classList.add('active')
     const rowBgSelect = e.currentTarget.parentNode.childNodes
     for (let i = 0; i < rowBgSelect.length; i++) {
       rowBgSelect[i].classList.add('alignInicator')
     }
   }
 
-  handleSlideLeft = () => {
-    const forAllselect = document.querySelectorAll('.forAllselect')
-
+  handleSlideLeft = async () => {
+    const backDateContainer = document.querySelectorAll('.backDateContainer')
+    const priceContainer = document.querySelectorAll('.priceContainer')
+    const nowSlide = this.state.nowInfo
+    console.log(priceContainer)
     // 不同列的變色
-    function controlAllEle() {
-      for (let i = 0; i < forAllselect.length; i++) {
-        // // 在觸發變色前先將所有上一狀態的顏色清除
-        // forAllselect[i].classList.remove('alignInicator', 'active')
+    if (nowSlide < 4) {
+      this.setState({ nowInfo: nowSlide + 1 })
+      console.log(nowSlide)
+      console.log(this.state.nowInfo)
+    }
+    function controlAllEle(e) {
+      for (let i = 0; i < e.length; i++) {
+        // // 在觸發變色前先將所有上一狀態的顏色清除Y
+        console.log(e[i])
+        // e[i].classList.remove('slide' + (nowSlide - 1))
+        e[i].classList.add('slide' + nowSlide)
         // 與點到的di於不同列相同index的變色
-        if (i % 7 > 3) {
-          forAllselect[i].classList.add('hide')
-        }
+        // if (i % 7 > 3) {
+        // }
       }
     }
+    controlAllEle(priceContainer)
+    await controlAllEle(backDateContainer)
   }
 
-  hadleMobleShowControl = () => {
-    console.log(document.body.clientWidth)
-    const forAllselect = document.querySelectorAll('.forAllselect')
-    const dateIntervalTop = document.querySelectorAll('.dateIntervalTop')
-    const mobileShow = this.state.show
-    if (document.body.clientWidth < 800) {
-      function mobileShowControl(ele) {
-        for (let i = 0; i < ele.length; i++) {
-          if (i % 7 > mobileShow) {
-            ele[i].classList.add('hide')
-          }
-        }
-      }
-      mobileShowControl(forAllselect)
-      mobileShowControl(dateIntervalTop)
-    } else {
-      function mobileShowControl(ele) {
-        for (let i = 0; i < ele.length; i++) {
-          if (i % 7 > mobileShow) {
-            ele[i].classList.remove('hide')
-          }
-        }
-      }
-      mobileShowControl(forAllselect)
-      mobileShowControl(dateIntervalTop)
-    }
-  }
+  // hadleMobleShowControl = () => {
+  //   console.log(document.body.clientWidth)
+  //   const forAllselect = document.querySelectorAll('.forAllselect')
+  //   const dateIntervalTop = document.querySelectorAll('.dateIntervalTop')
+  //   const mobileShow = this.state.show
+  //   if (document.body.clientWidth < 800) {
+  //     function mobileShowControl(ele) {
+  //       for (let i = 0; i < ele.length; i++) {
+  //         if (i % 7 > mobileShow) {
+  //           ele[i].classList.add('hide')
+  //         }
+  //       }
+  //     }
+  //     mobileShowControl(forAllselect)
+  //     mobileShowControl(dateIntervalTop)
+  //   } else {
+  //     function mobileShowControl(ele) {
+  //       for (let i = 0; i < ele.length; i++) {
+  //         if (i % 7 > mobileShow) {
+  //           ele[i].classList.remove('hide')
+  //         }
+  //       }
+  //     }
+  //     mobileShowControl(forAllselect)
+  //     mobileShowControl(dateIntervalTop)
+  //   }
+  // }
 
   render() {
     return (
@@ -115,7 +121,7 @@ class Mainpage extends React.Component {
         <div className="wrapper">
           <table>
             <tbody>
-              {/* <button>123</button> */}
+              <button onClick={this.handleSlideLeft}>123</button>
               <tr>
                 <td className="firsTd">
                   <div className="categoryContainer">
@@ -127,10 +133,10 @@ class Mainpage extends React.Component {
                 </td>
 
                 <td className="seconTd">
-                  <div className="backDateContainer ">
+                  <div className="backDateContainer slide0">
                     {this.state.Info.map((ele, index) => (
                       <div
-                        className="dateIntervalTop "
+                        className={'dateIntervalTop cell' + this.state.show}
                         key={index + +new Date()}
                       >
                         <span>{ele.goDate}</span>
@@ -150,12 +156,13 @@ class Mainpage extends React.Component {
                     </div>
                   </td>
                   <td className="seconTd">
-                    <div className="priceContainer">
+                    <div className="priceContainer  ">
                       {ele.detail.map((e, index) => (
                         <div
                           className={
                             (e.cheapest === true ? 'cheapest' : '') +
-                            'cellBox forAllselect'
+                            ' forAllselect cell' +
+                            this.state.show
                           }
                           key={index + +new Date()}
                           onMouseEnter={this.tableContentMouseEnter}
